56cb18050bfbf9e65e2ea8b5d2751ca303786792
Eli Sklar <eli.sklar@gmail.com>
2020-06-29 03:50:42 +0300

    Added support for eslint

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-function modify(baseConfig, { target, dev }, webpack, userOptions = {}) {
-	 const config = Object.assign({}, baseConfig);
-	 
-  config.resolve.extensions.push(".ts", ".tsx");
+const ForkTSCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
+const path = require('path')
+
+function modify(defaultConfig, { target, dev }, webpack, userOptions = {}) {
+  const config = defaultConfig
+  config.resolve.extensions.push('.ts', '.tsx')
+  config.plugins.push(
+    new ForkTSCheckerWebpackPlugin({
+      checkSyntacticErrors: true,
+      eslint: true,
+  }))
   config.module.rules.push({
-    // Include ts, tsx
     test: /\.(ts|js)x?$/,
-    exclude: /node_modules/,
-    loader: "babel-loader"
-  });
-  return config;
+    include: [path.resolve(__dirname, 'src')],
+    loader: 'babel-loader',
+  })
+  return config
 }
 
 module.exports = modify;
